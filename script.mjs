f5d12fc600e5eefec5f75404dcd3815025ac8e8d
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-01-15 13:13:07 +0100

    Lagt til dikt på adresse /tmd/poem

--- script.mjs
+++ script.mjs
@@ -1,18 +1,38 @@
-import express from 'express'
-import HTTP_CODES from './utils/httpCodes.mjs';
+import express from "express";
+import HTTP_CODES from "./utils/httpCodes.mjs";
 
 const server = express();
-const port = (process.env.PORT || 8000);
+const port = process.env.PORT || 8080;
+const quotes = []
+const poem = `Lykke<br><br>
+Hva lykke er?<br>
+Gå på en gressgrodd setervei<br>
+i tynne, tynne sommerklær,<br>
+klø sine ferske myggstikk<br>
+med doven ettertenksomhet<br>
+og være ung og meget rik<br>
+på uopplevet kjærlighet.<br>
+Å få et florlett spindelvev<br>
+som kjærtegn over munn og kinn<br>
+og tenke litt på vær og vind.<br>
+Be prestekravene om råd<br>
+og kanskje ja – og kanskje nei –<br>
+han elsker – elsker ikke meg.<br>
 
-server.set('port', port);
-server.use(express.static('public'));
+Men ennå ikke kjenne deg.<br>`;
+
+server.set("port", port);
+server.use(express.static("public"));
 
 function getRoot(req, res, next) {
-    res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
+  res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
 }
 
 server.get("/", getRoot);
+server.get("/tmp/poem", (req, res) => {
+  res.send(poem);
+});
 
-server.listen(server.get('port'), function () {
-    console.log('server running', server.get('port'));
+server.listen(server.get("port"), function () {
+  console.log("server running", server.get("port"));
 });
