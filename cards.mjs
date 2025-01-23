22f310f2073ceebec0e4d16909df2eef7092b917
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-01-23 13:06:14 +0100

    La til funksjon for å lage ny kortstokk med ID ved POST-req på /temp/deck + laget klient-side med knapper

--- cards.mjs
+++ cards.mjs
@@ -1,2 +1,40 @@
-const suits = ['hjerter', 'ruter', 'kløver', 'spar'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+import { v4 as uuidv4 } from "uuid";
+
+function makeNewDeck() {
+  const suits = ["hjerter", "ruter", "kløver", "spar"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+
+  const deck = suits.flatMap((suit) =>
+    values.map((value) => ({ value, suit }))
+  );
+  const deckWithID = { id: uuidv4(), cards: deck };
+  console.log(deckWithID);
+  return deckWithID;
+}
+
+function shuffleDeck(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+  console.log(deck);
+  return deck;
+}
+
+export { makeNewDeck, shuffleDeck };
--- script.mjs
+++ script.mjs
@@ -1,37 +1,12 @@
 import express from "express";
 import HTTP_CODES from "./utils/httpCodes.mjs";
-import * as cards from "cards.mjs";
+import { quotes, poem } from "./quotes_poem.mjs";
+import { makeNewDeck, shuffleDeck } from "./cards.mjs";
+import { v4 as uuidv4 } from "uuid"; 
+// import * as cards from "./cards.mjs";
 
 const server = express();
 const port = process.env.PORT || 8080;
-const quotes = [
-    "Be yourself; everyone else is already taken.<br>- Oscar Wilde",
-    "So many books, so little time<br>- Frank Zappa", 
-    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.<br>- Albert Einstein",
-    "Be the change that you wish to see in the world.<br>- Mahatma Gandhi",
-    "In three words I can sum up everything I've learned about life: it goes on.<br>- Robert Frost",
-    "If you tell the truth, you don't have to remember anything.<br>- Mark Twain",
-    "A room without books is like a body without a soul.<br>- Marcus Tullius Cicero",
-    "You only live once, but if you do it right, once is enough.<br>- Mae West",
-    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.<br>- Ralph Waldo Emerson",
-    "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.<br>- Maya Angelou"];
-
-const poem = `Lykke<br><br>
-Hva lykke er?<br>
-Gå på en gressgrodd setervei<br>
-i tynne, tynne sommerklær,<br>
-klø sine ferske myggstikk<br>
-med doven ettertenksomhet<br>
-og være ung og meget rik<br>
-på uopplevet kjærlighet.<br>
-Å få et florlett spindelvev<br>
-som kjærtegn over munn og kinn<br>
-og tenke litt på vær og vind.<br>
-Be prestekravene om råd<br>
-og kanskje ja – og kanskje nei –<br>
-han elsker – elsker ikke meg.<br>
-
-Men ennå ikke kjenne deg.<br>`;
 
 server.set("port", port);
 server.use(express.static("public"));
@@ -40,7 +15,6 @@ server.use(express.json());
 function getRoot(req, res, next) {
   res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
 }
-
 
 server.get("/", getRoot);
 server.get("/tmp/poem", (req, res) => {
@@ -51,11 +25,24 @@ server.get("/tmp/quote", (req, res) => {
 });
 
 server.post("/tmp/sum/:a/:b", (req, res) => {
-    
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
-    const sum = a + b;
-    res.send(sum.toString());
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+  const sum = a + b;
+  res.send(sum.toString());
+});
+
+server.post("/temp/deck", (req, res) => {
+  const deck = makeNewDeck();
+  res.send(deck);
+});
+
+server.patch("/temp/deck/shuffle/:deck_id", (req, res) => {
+  const { cards } = req.body;
+  if (!cards) {
+    return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No cards found");
+  }
+  const shuffledDeck = shuffleDeck(req.body.cards);
+  res.send(shuffledDeck);
 });
 
 server.listen(server.get("port"), function () {
