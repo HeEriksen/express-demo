1f4f11137d430542a338ddb767854f5c5970bb7d
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-01-23 15:27:48 +0100

    Endret navn på html fil

--- script.mjs
+++ script.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import HTTP_CODES from "./utils/httpCodes.mjs";
 import { quotes, poem } from "./quotes_poem.mjs";
 import { makeNewDeck, shuffleDeck } from "./cards.mjs";
@@ -6,6 +7,8 @@ import { v4 as uuidv4 } from "uuid";
 
 const server = express();
 const port = process.env.PORT || 8080;
+const __dirname = path.resolve();
+
 const decks = {};
 
 server.set("port", port);
@@ -15,8 +18,10 @@ server.use(express.json());
 function getRoot(req, res, next) {
   res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
 }
-
 server.get("/", getRoot);
+
+
+
 server.get("/tmp/poem", (req, res) => {
   res.send(poem);
 });
@@ -30,6 +35,13 @@ server.post("/tmp/sum/:a/:b", (req, res) => {
   const sum = a + b;
   res.send(sum.toString());
 });
+
+const serveDrawCards = (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "drawCards.html"));
+};
+
+server.get("/temp/deck*", serveDrawCards);
+
 
 server.post("/temp/deck", (req, res) => {
   const deck = makeNewDeck();
@@ -48,6 +60,16 @@ server.patch("/temp/deck/shuffle/:deck_id", (req, res) => {
   }
   const shuffledDeck = shuffleDeck(cards);
   res.send({ id: deckID, cards: shuffledDeck });
+});
+
+server.get("/temp/deck/:deck_id", (req, res) => {
+  const deckID = req.params.deck_id;
+  console.log("getting deck with ID:", deckID);
+  const cards = decks[deckID];
+  if (!cards) {
+    return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No cards found");
+  }
+  res.send(decks[deckID]);
 });
 
 server.listen(server.get("port"), function () {
