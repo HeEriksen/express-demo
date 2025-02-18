abe4f04636f235ddbd2f45e8e601ee2ce8bc315c
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-02-18 14:02:56 +0100

    La til fil for custom session middleware

---
+++ modules/sessions.mjs
@@ -0,0 +1,20 @@
+// const SESSIONS = {};
+// const SESSION_KEY = "session";
+
+// function startSession(req,res,next){
+//     let sessionId = req.get(SESSION_KEY)
+// let session SESSIONS[sessionId]
+
+// if(!sessionId){
+//     sessionId = createUniqueSessionId(16,SESSIONS);
+// }
+
+// function createUniqueSessionId(length, sessions){
+//         let id = "";
+//         do {
+//             id = crypto.randomBytes(length).toString("hex");
+//         } while (sessions[id] != undefined);
+//     }
+// }};
+
+// export default startSession;
