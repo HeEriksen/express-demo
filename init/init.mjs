0f3f6dc15b558720ce7a78cf2aa26cb99675778f
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-02-18 17:08:14 +0100

    la til kode fra forelesning i init og fil til dummy data

--- init/init.mjs
+++ init/init.mjs
@@ -1,21 +1,15 @@
-import { Node, Tree } from "../data/tree.mjs";
+import { Node, Tree, saveTree } from "../data/tree.mjs"
+import fs from "fs/promises"
 
-//#region DUMMY data ------------------------------------
-const a1 = Node("A1", []);
-const a2 = Node("A2", []);
-const a = Node("A", a1, a2);
+//#region DUMMY data --------------------
 
-
-const b1 = Node("B1", []);
-const b2 = Node("B2", []);
-const b = Node("B", b1, b2);
-
-const root = Node("data", a, b);
-const tree = Tree(root);
-
-console.log(saveTree(root));
-//#endregion 
+let treeData = await fs.readFile("./init/dummy/tree1.json");
+console.log(treeData);
 
 
-// Start server ----------------------------------------
-const server = await import("../server.mjs");
+
+//#endregion
+
+
+// Start server ----------
+const server = await import("../server.mjs")
