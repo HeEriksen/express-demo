c83b104b24be6a67fe5d1e455f0815a90cd32421
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-02-18 14:17:44 +0100

    La til kode for session middleware

--- modules/sessions.mjs
+++ modules/sessions.mjs
@@ -1,20 +1,34 @@
-// const SESSIONS = {};
-// const SESSION_KEY = "session";
 
-// function startSession(req,res,next){
-//     let sessionId = req.get(SESSION_KEY)
-// let session SESSIONS[sessionId]
+const SESSION_KEY = "session";
+const SESSIONS = {};
 
-// if(!sessionId){
-//     sessionId = createUniqueSessionId(16,SESSIONS);
-// }
+function startSession(req, res, next) {
 
-// function createUniqueSessionId(length, sessions){
-//         let id = "";
-//         do {
-//             id = crypto.randomBytes(length).toString("hex");
-//         } while (sessions[id] != undefined);
-//     }
-// }};
+    let sessionId = req.get(SESSION_KEY);
+    let session = SESSIONS[sessionId];
 
-// export default startSession;
+    if (!sessionId) {
+        sessionId = createUniqueSessionId(20, SESSIONS);
+        session = { id: sessionId };
+        SESSIONS[sessionId] = session;
+    }
+
+    res.set(SESSION_KEY, sessionId);
+    req.session = session;
+    next();
+}
+
+function updateSession(req, res, next) {
+    SESSIONS[req.session.id] = req.session;
+    next();
+}
+
+function createUniqueSessionId(length, sessions) {
+    let id = "";
+    do {
+        id = crypto.randomBytes(length).toString("hex");
+    } while (sessions[id] != undefined)
+    return id;
+}
+
+export { startSession, updateSession };
