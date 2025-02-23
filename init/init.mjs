e994256fe964b0123edfb506e6bcbfbe645894a4
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-02-23 11:41:32 +0100

    lagt inn kode for dummy tre

--- init/init.mjs
+++ init/init.mjs
@@ -3,9 +3,22 @@ import fs from "fs/promises"
 
 //#region DUMMY data --------------------
 
-let treeData = await fs.readFile("./init/dummy/tree1.json");
-console.log(treeData);
+const a1 = Node("A1", []);
+const a2 = Node("A2", []);
+const a = Node("A", a1, a2);
 
+
+const b1 = Node("B1", []);
+const b2 = Node("B2", []);
+const b = Node("B", b1, b2);
+
+const root = Node("data", a, b);
+export const tree = Tree(root);
+
+console.log(saveTree(root));
+
+// let treeData = await fs.readFile("./init/dummy/tree1.json");
+// console.log(treeData);
 
 
 //#endregion
