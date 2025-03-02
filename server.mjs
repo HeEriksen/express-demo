1b98e88e29e7cec61259686022422699d1dce598
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-02 01:26:39 +0100

    OBS! Fjerner det som ikke lenger er relevant (tidligere innleveringer)

--- server.mjs
+++ server.mjs
@@ -65,68 +65,6 @@ function getRoot(req, res, next) {
 
 server.get("/", getRoot);
 
-server.get("/tmp/poem", (req, res) => {
-  res.send(poem);
-});
-
-server.get("/tmp/quote", (req, res) => {
-  res.send(quotes[Math.floor(Math.random() * quotes.length)]);
-});
-
-server.post("/tmp/sum/:a/:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-  const sum = a + b;
-  res.send(sum.toString());
-});
-
-const serveDrawCards = (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "drawCards.html"));
-};
-
-server.get("/temp/deck", serveDrawCards);
-
-server.post("/temp/deck", (req, res) => {
-  const deck = makeNewDeck();
-  decks[deck.id] = deck.cards;
-  res.send(deck);
-});
-
-server.patch("/temp/deck/shuffle/:deck_id", (req, res) => {
-  const deckID = req.params.deck_id;
-  const cards = decks[deckID];
-  if (!cards) {
-    return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No cards found");
-  }
-  const shuffledDeck = shuffleDeck(cards);
-  res.send({ id: deckID, cards: shuffledDeck });
-});
-
-server.get("/temp/deck/:deck_id", (req, res) => {
-  const deckID = req.params.deck_id;
-  const cards = decks[deckID];
-  if (!cards) {
-    return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No cards found");
-  }
-  res.json(cards);
-});
-
-server.get("/temp/deck/:deck_id/card", (req, res) => {
-  const deckID = req.params.deck_id;
-  const cards = decks[deckID];
-  const drawnCard = drawCard(cards);
-  const img = drawnCard.value + "_of_" + drawnCard.suit + ".png";
-
-  if (!cards) {
-    return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No cards found");
-  }
-  res.send(drawnCard);
-});
-
 server.listen(server.get("port"), function () {
   console.log("server running", server.get("port"));
-});
-
-server.get("/tmp/jobbquest", (req, res) => {
-  res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
 });
