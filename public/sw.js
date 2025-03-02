10c688d0d9c7cfcd4ca0910575bfe2bf628a73f1
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-02 15:03:54 +0100

    Endret skrivefeil

--- public/sw.js
+++ public/sw.js
@@ -8,29 +8,29 @@ const contentToCache = [
 ];
 
 self.addEventListener("install", (evt) => {
-    console.log('service worker has been installed');
-    e.waitUntil((async () => {
+    console.log('[Service worker] installed');
+    evt.waitUntil((async () => {
         const cache = await caches.open(cacheID);
         console.log('[service worker] Caching all: app shell and content')
         await cache.addAll(contentToCache);
     }))
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (evt) => {
     if (!(
-        e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+        evt.request.url.startsWith('http:') || evt.request.url.startsWith('https:')
     )) {
         return;
     }
 
     e.respondWith((async () => {
-        const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        const r = await caches.match(evt.request);
+        console.log(`[Service Worker] Fetching resource: ${evt.request.url}`);
         if (r) { return r };
-        const response = await fetch(e.request);
+        const response = await fetch(evt.request);
         const cache = await caches.open(cacheID);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
+        console.log(`[Service Worker] Caching new resource: ${evt.request.url}`);
+        cache.put(evt.request, response.clone());
         return response;
     })());
 });
