1536192ecbed22860a84e96a60fd945948392c79
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-06 15:09:34 +0100

    La til ny statuskode for 201 (CREATED), for når noe lages med POST req.

--- utils/httpCodes.mjs
+++ utils/httpCodes.mjs
@@ -3,7 +3,8 @@ import { setUncaughtExceptionCaptureCallback } from "node:process"
 const HTTP_CODES = {
 
     SUCCESS: {
-        OK: 200
+        OK: 200, 
+        CREATED: 201
     },
     CLIENT_ERROR: {
         NOT_FOUND: 404
