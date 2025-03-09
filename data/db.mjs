98649daae43a5d427e9770ae91e4ab9803a238f3
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-09 18:55:22 +0100

    La til fil + kode  for database query

---
+++ data/db.mjs
@@ -0,0 +1,46 @@
+import { query } from "express";
+import pg from "pg";
+const { Client } = pg;
+
+const config = {
+  connectionString: process.env.DB_CREDENTIALS,
+  ssl: process.env.DB_SSL === "true" ? process.env.DB_SSL : false,
+};
+
+async function create(statement, ...values) {
+    return await runQuery(statement, ...values);
+}
+
+async function update(statement, ...values) {
+    return await runQuery(statement, ...values);
+}
+
+async function read(statement, ...values) {
+    return await runQuery(statement, ...values);
+}
+
+async function remove(statement, ...values) {
+    return await runQuery(statement, ...values);
+}
+
+
+
+async function runQuery(query, ...values) {
+  const client = new Client(config);
+
+  try {
+    client.connect();
+    const result = client.query(statement, [...values]);
+
+    if (result.rowcount <= 0) {
+      throw new Error("No records created");
+    }
+
+    return result.row[0];
+  } catch (error) {
+    console.error(error);
+    return null;
+  } finally {
+    client.close();
+  }
+}
