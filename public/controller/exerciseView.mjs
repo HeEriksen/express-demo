7301f299f6adce8bc6b59d7c0b40d383ec078248
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-11 15:36:31 +0100

    la til filer for views + controllere, ikke laget kode

---
+++ public/controller/exerciseView.mjs
@@ -0,0 +1,16 @@
+// import TemplateManager from "../modules/templateManager.mjs";
+// const templateFile = "./views/exerciseView.html";
+
+// const template = TemplateManager.fetchTemplate(templateFile);
+// const exerciseView = TemplateManager.cloneTemplate(template);
+
+
+// exerciseView.getElementById("button").onClick = (evt) => {
+//     console.log("Yesss");
+// }
+
+// exeerciseViewController = {
+//     view: exerciseView
+// };
+
+// export default exerciseViewController
