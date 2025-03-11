6538b2443625a841b1aab476eba7325c63debfd9
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-11 15:40:25 +0100

    Endret kode i router til å bruke kode fra workout.mjs til CRUD

--- routes/workoutsAPI.mjs
+++ routes/workoutsAPI.mjs
@@ -1,32 +1,62 @@
 import express from "express";
 import HTTP_CODES from "../utils/httpCodes.mjs";
 import { v4 as uuidv4 } from "uuid";
+import Workout from "../data/workout.mjs";
+
 const workoutsRouter = express.Router();
-let workouts = [];
 
 workoutsRouter.use(express.json());
 
-workoutsRouter.get("/", (req, res, next) => res.json(workouts));
-
-workoutsRouter.post("/", (req, res, next) => {
-  const newWorkout = { id: uuidv4(), date: req.body.date, exercises: [] };
-  workouts.push(newWorkout);
-  res.status(HTTP_CODES.SUCCESS.CREATED).json(newWorkout);
+workoutsRouter.get("/", async (req, res, next) => {
+  try {
+    const workouts = await new Workout().read();
+    res.json(workouts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-workoutsRouter.put("/", (req, res, next) => {});
+workoutsRouter.post("/", async (req, res, next) => {
+  try {
+    const newWorkout = new Workout(
+      null,
+      uuidv4(),
+      req.body.date,
+      req.body.exercises || []
+    );
+    const createdWorkout = await newWorkout.create();
+    res.status(HTTP_CODES.SUCCESS.CREATED).json(createdWorkout);
+  } catch (error) {
+    next(error);
+  }
+});
 
-workoutsRouter.delete("/:id", (req, res, next) => {
-  const id = req.params.id;
-  const originalArrayLength = workouts.length;
+workoutsRouter.put("/:id", async (req, res, next) => {
+  try {
+    let workout = new Workout(null, req.params.id)
+    workout = await workout.read();
+    workout.date = req.body.date || workout.date;
+    workout.workout = req.body.exercises || workout.workout;
+    const updatedWorkout = await workout.update();
+    res.json(updatedWorkout);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  workouts = workouts.filter(workout => workout.id !== id);
-  if (workouts.length < originalArrayLength) {
-    res.json({ message: "Treningsøkt ble slettet" });
-  } else {
-    res
-      .status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND)
-      .json({ error: "Kunne ikke finne treningsøkt" });
+workoutsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const workout = new Workout(null, req.params.id);
+    const result = await workout.purge();
+    if (result === null) {
+      res.json({ message: "Treningsøkt ble slettet" });
+    } else {
+      res
+        .status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND)
+        .json({ error: "Kunne ikke finne treningsøkten" });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
