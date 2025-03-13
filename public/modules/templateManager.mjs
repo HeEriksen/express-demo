65dd61740dfa53ce1fe9853602fa7351b4aecd26
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-13 16:35:54 +0100

    la inn error for feilsøking + endret RegEx

--- public/modules/templateManager.mjs
+++ public/modules/templateManager.mjs
@@ -1,29 +1,30 @@
 const TemplateManager = {};
 
 TemplateManager.fetchTemplate = async (path) => {
-
-    // Laster inn extern template
     let rawTemplate = await (await fetch(path)).text();
-    // Et hack for å gjøre det enklere å laste templates dynamisk. 
     let div = document.createElement("div");
     div.innerHTML = rawTemplate;
-    let template = div.firstChild;
+    let template = div.querySelector("template");
+    if (!template) {
+        throw new Error("Template not found or invalid template format");
+    }
     return template;
+};
 
-}
-
-TemplateManager.cloneTemplate = (template, target, data) => {
+TemplateManager.cloneTemplate = (template, target = document.body, data = {}) => {
+    if (!template || !template.content) {
+        throw new Error("Invalid template provided");
+    }
     const clone = template.content.cloneNode(true);
     let html = clone.innerHTML;
 
     for (let key of Object.keys(data)) {
-        html = html.replaceAll(RegExp(`/\{\{${key}\}\}/gm`, data[key]));
+        html = html.replace(new RegExp(`\\{\\{${key}\\}\\}`, "g"), data[key]);
     }
 
     clone.innerHTML = html;
     target.appendChild(clone);
     return clone;
-}
+};
 
-
-export default TemplateManager
+export default TemplateManager;
