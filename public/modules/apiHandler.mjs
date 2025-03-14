73fce012661e6866c8b48de4865e508a0149a5dc
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-14 04:28:17 +0100

    prod = false

--- public/modules/apiHandler.mjs
+++ public/modules/apiHandler.mjs
@@ -5,7 +5,7 @@ const HTTP_METHODS = {
   PUT: "PUT",
 };
 
-const isPROD = true;
+const isPROD = false;
 const BASE_API_TEST = "http://localhost:8080/";
 const BASE_API_PROD = "https://express-demo-b43z.onrender.com/";
 
