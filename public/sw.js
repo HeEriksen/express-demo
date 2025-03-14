8237ad6ebf644bb7931ae783a1f44d9e93c63b50
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-14 02:59:57 +0100

    la til if-test på linker som inneholder /api/

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,61 @@
-const cacheID = "loadUpV1";
+const cacheID = "loadUpV2";
 const contentToCache = [
-    "/index.html",
-    "/styles.css",
-    "/app.mjs",
-    "/icons/training_128.png",
-    "/icons/training_512.png"
+  "/index.html",
+  "/styles.css",
+  "/app.mjs",
+  "/icons/training_128.png",
+  "/icons/training_512.png",
 ];
 
 self.addEventListener("install", (evt) => {
-    console.log('[Service worker] installed');
-    evt.waitUntil((async () => {
-        const cache = await caches.open(cacheID);
-        console.log('[service worker] Caching all: app shell and content')
-        await cache.addAll(contentToCache);
-    }))
+  console.log("[Service worker] installed");
+  evt.waitUntil(async () => {
+    const cache = await caches.open(cacheID);
+    console.log("[service worker] Caching all: app shell and content");
+    await cache.addAll(contentToCache);
+  });
 });
 
-self.addEventListener('fetch', (evt) => {
-    if (!(
-        evt.request.url.startsWith('http:') || evt.request.url.startsWith('https:')
-    )) {
-        return;
-    }
+self.addEventListener("fetch", (evt) => {
+  if (
+    !(
+      evt.request.url.startsWith("http:") ||
+      evt.request.url.startsWith("https:")
+    )
+  ) {
+    return;
+  }
 
-    evt.respondWith((async () => {
-        const r = await caches.match(evt.request);
-        console.log(`[Service Worker] Fetching resource: ${evt.request.url}`);
-        if (r) { return r; }
-        try {
-            const response = await fetch(evt.request);
-            const cache = await caches.open(cacheID);
-            console.log(`[Service Worker] Caching new resource: ${evt.request.url}`);
-            cache.put(evt.request, response.clone());
-            return response;
-        } catch (error) {
-            console.error(`[Service Worker] Fetch failed for: ${evt.request.url}`, error);
-            throw error;
-        }
-    })());
+  if (evt.request.url.includes("/api/")) {
+    return fetch(evt.request);
+  }
+
+  if (evt.request.method !== "GET") {
+    return fetch(evt.request);
+  }
+
+  evt.respondWith(
+    (async () => {
+      const cachedResponse = await caches.match(evt.request);
+      console.log(`[Service Worker] Fetching resource: ${evt.request.url}`);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      try {
+        const response = await fetch(evt.request);
+        const cache = await caches.open(cacheID);
+        console.log(
+          `[Service Worker] Caching new resource: ${evt.request.url}`
+        );
+        cache.put(evt.request, response.clone());
+        return response;
+      } catch (error) {
+        console.error(
+          `[Service Worker] Fetch failed for: ${evt.request.url}`,
+          error
+        );
+        throw error;
+      }
+    })()
+  );
 });
