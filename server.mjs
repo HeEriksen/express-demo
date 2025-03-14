2145586f3de26b10929701020e0819688793b9d2
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-14 09:56:48 +0100

    sletter unødvendig kode + filer siden jeg ikke kommer meg videre fra problemer med databasen og mest sannsynlig ikke rekker å begynne på disse

--- server.mjs
+++ server.mjs
@@ -1,14 +1,10 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
-import HTTP_CODES from "./utils/httpCodes.mjs";
 import log from "./modules/log.mjs";
 import { LOGG_LEVELS, eventLogger } from "./modules/log.mjs";
 import abTest from "./modules/abTesting.mjs";
 import workoutsRouter from "./routes/workoutsAPI.mjs";
-
-
-// // import session from "./modules/sessions.mjs";
 
 const ENABLE_LOGGING = false;
 
@@ -25,33 +21,6 @@ server.use(abTest);
 
 server.use("/api/workouts", workoutsRouter);
 server.use(express.static("public"));
-// TODO: flytt ab-test relaterte funksjoner ut i modul-mappe der det er mulig
-
-// function getRootA(req, res, next) {
-//   if (req.abVariant === "A") {
-//     eventLogger(
-//       "Noen spurte etter root|Brukeren fikk variant: " + req.abVariant
-//     );
-//     res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
-//   } else {console.log(abVariant);}
-// }
-
-// function getRootB(req, res, next) {
-//   if (req.abVariant === "B") {
-//     eventLogger(
-//       "Noen spurte etter root|Brukeren fikk variant: " + req.abVariant
-//     );
-//     res.status(HTTP_CODES.SUCCESS.OK).send("Hello Universe").end();
-//   } else {console.log(abVariant);}
-// }
-
-// function ABTestTarget(config) {
-//   return (req, res, next) => {
-//     config[req.abVariant]();
-//   };
-// }
-
-// server.get("/", ABTestTarget({ A: getRootA, B: getRootB }));
 
 server.listen(server.get("port"), function () {
   console.log("server running", server.get("port"));
