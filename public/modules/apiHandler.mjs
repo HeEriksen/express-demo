188d3f12c21258e5b692c7a64773fe51a28c0524
HeEriksen <Helene.R.Eriksen@gmail.com>
2025-03-14 03:04:07 +0100

    endret kode sånn at http metode kommer med + returnering

--- public/modules/apiHandler.mjs
+++ public/modules/apiHandler.mjs
@@ -13,9 +13,9 @@ const BASE_API = isPROD ? BASE_API_PROD : BASE_API_TEST;
 
 const API_ENDPOINTS_TRAINING_SERVER = {
   GetWorkouts: `${BASE_API}api/workouts`,
-  AddWorkout: `${BASE_API}api/workouts`,
-  UpdateWorkout: (id) => `${BASE_API}api/workouts${id}`,
-  DeleteWorkout: (id) => `${BASE_API}api/workouts${id}`,
+  CreateWorkout: `${BASE_API}api/workouts`,
+  UpdateWorkout: (id) => `${BASE_API}api/workouts/${id}`,
+  DeleteWorkout: (id) => `${BASE_API}api/workouts/${id}`,
 };
 
 async function getWorkouts(pwaID) {
@@ -24,22 +24,30 @@ async function getWorkouts(pwaID) {
   );
 }
 
-async function addWorkout(workoutID) {
-  const workout = await runRequest(
-    API_ENDPOINTS_TRAINING_SERVER.AddWorkout(workoutID)
+async function addWorkout(workout) {
+  const updatedWorkout = await runRequest(
+    API_ENDPOINTS_TRAINING_SERVER.CreateWorkout,
+    HTTP_METHODS.POST,
+    workout
   );
+  return updatedWorkout;
 }
 
-async function updateWorkout(workoutID) {
-  const workout = await runRequest(
-    API_ENDPOINTS_TRAINING_SERVER.UpdateWorkout(workoutID)
+async function updateWorkout(workoutID, updatedWorkoutData) {
+  const updatedWorkout = await runRequest(
+    API_ENDPOINTS_TRAINING_SERVER.UpdateWorkout(workoutID),
+    HTTP_METHODS.PUT,
+    updatedWorkoutData
   );
+  return updatedWorkout;
 }
 
 async function deleteWorkout(workoutID) {
-  const workout = await runRequest(
-    API_ENDPOINTS_TRAINING_SERVER.DeleteWorkout(workoutID)
+  const result = await runRequest(
+    API_ENDPOINTS_TRAINING_SERVER.DeleteWorkout(workoutID),
+    HTTP_METHODS.DELETE
   );
+  return result;
 }
 
 async function runRequest(path, method = HTTP_METHODS.GET, data = null) {
@@ -62,7 +70,6 @@ async function runRequest(path, method = HTTP_METHODS.GET, data = null) {
 }
 
 console.log(`API is running ${isPROD ? "PROD" : "Test"} `);
-
 
 export {
   HTTP_METHODS,
